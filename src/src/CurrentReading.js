1800948922f8f3e0b4681839ae9ee0d1fefc7543
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-07-11 11:30:12 -0400

    Add First Components
    Starter code for the components to handle each of the main three sections of the page.

---
+++ src/src/CurrentReading.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+
+class CurrentReading extends Component {
+    render() {
+    return <div>Currently Reading Component{this.props.name}</div>;
+  }
+}
+
+export default CurrentReading
